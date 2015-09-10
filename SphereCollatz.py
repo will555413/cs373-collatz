8df12e063c2848d905e927fdc3f7882ee0df5f84
William <will555413>
2015-09-10 17:43:48 -0500

    SphereCollatz.py updated and accepted

--- SphereCollatz.py
+++ SphereCollatz.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-# ------------------------------
-# projects/collatz/RunCollatz.py
+# ---------------------------
+# projects/collatz/Collatz.py
 # Copyright (C) 2015
 # Glenn P. Downing
-# ------------------------------
+# ---------------------------
 
 # -------
 # imports
 # -------
 
 import sys
+
+# ------------
+# Global Cache
+# ------------
+
+cache = []
 
 # ------------
 # collatz_read
@@ -39,25 +45,47 @@ def collatz_eval (i, j) :
     assert i>0 and i<=1000000
     assert j>0 and j<=1000000
 
-    b = j if j > i  else i
+    b = j if j>i else i
     a = i if b == j else j
     a = b//2 if b//2>a else a
-    
-    
+
     max_count = -1
-    # Since python range() is exclusive on the end, I extended the range by 1 to make it arbitrarily inclusive
     for num in range(a, b+1):
-        count = 1
-        while(num > 1):
-            if num % 2 == 1:
-                num = num * 3 + 1
-            else:
-                num = num // 2
-            count += 1
+        count = cycle_length(num)
         if count > max_count:
             max_count = count
-    assert max_count >= 1
+
+    assert max_count > 0
     return max_count
+
+# ------------
+# cycle_length
+# ------------
+
+def cycle_length (num):
+    """
+    returns the cycle length of num
+    if cache have stored a valid value, return it
+    if not, get the next number in sequence and call itself recursively
+    at each recursion, if num is a storable number (depends on size of cache), stored the value of return by the recursive call
+    """
+    global cache
+    if num < 999999 and num > -1 and cache[num] > 0:
+        return cache[num]
+    elif num % 2 == 1:
+        next_num = num + (num // 2) + 1
+        if num < 999999 and num > -1:
+            cache[num] = 2 + cycle_length(next_num)
+            return cache[num]
+        else:
+            return 2 + cycle_length(next_num)
+    else:
+        next_num = num // 2
+        if num < 999999 and num > -1:
+            cache[num] = 1 + cycle_length(next_num)
+            return cache[num]
+        else:
+            return 1 + cycle_length(next_num)
 
 # -------------
 # collatz_print
@@ -82,10 +110,19 @@ def collatz_solve (r, w) :
     r a reader
     w a writer
     """
+    global cache
+    # manually added cycle length of 0, which is indexed at 0, as -1 (garbage value)
+    cache.append(-1)
+    # manually added cycle length of 1, which is indexed at 1, as 1
+    cache.append(1)
+    # add cycle length placeholders into the cache, starting from 2 since 0 and 1 have been added
+    for idx in range(2, 1000000):
+        cache.append(0)
     for s in r :
         i, j = collatz_read(s)
         v    = collatz_eval(i, j)
         collatz_print(w, i, j, v)
+
 
 # ----
 # main
